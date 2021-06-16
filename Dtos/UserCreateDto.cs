22325c24240b73b199e7e252af762ebb0fe54018
aldenpoole <62851994+aldenpoole@users.noreply.github.com>
2021-06-16 12:26:05 -0500

    Update UserCreateDto.cs

--- Dtos/UserCreateDto.cs
+++ Dtos/UserCreateDto.cs
@@ -1,14 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dwnStats.Dtos
 {
     public class UserCreateDto
     {
 
+        [Required]
         public string firstName {get;set;}
-
+        [Required]
         public string lastName {get;set;}
-
+        [Required]
         public string userName{get;set;}
-
+        [Required]
         public string password{get;set;}
 
         
