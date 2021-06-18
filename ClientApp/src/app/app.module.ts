068b934bcd2abfef9c55978fa66be8e78182e027
SamSongy-Parsons <sam.songy@parsons.com>
2021-06-18 10:15:11 -0500

    Add bootstrap, create dashboard component and its child components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,12 +9,22 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { HttpClientModule } from '@angular/common/http';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DataDownloadedGraphComponent } from './dashboard/data-downloaded-graph/data-downloaded-graph.component';
+import { UserAnalyticsComponent } from './dashboard/user-analytics/user-analytics.component';
+import { UserDownloadPiechartsComponent } from './dashboard/user-download-piecharts/user-download-piecharts.component';
+import { LatestActivityComponent } from './dashboard/latest-activity/latest-activity.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersTableComponent
+    UsersTableComponent,
+    DashboardComponent,
+    DataDownloadedGraphComponent,
+    UserAnalyticsComponent,
+    UserDownloadPiechartsComponent,
+    LatestActivityComponent
   ],
   imports: [
     BrowserModule,
---
+++ ClientApp/src/app/dashboard/data-downloaded-graph/data-downloaded-graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-downloaded-graph',
+  templateUrl: './data-downloaded-graph.component.html',
+  styleUrls: ['./data-downloaded-graph.component.css']
+})
+export class DataDownloadedGraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
