f29a6a8e662407aacd7fbbac5ce8c9fb9274ed6d
m-hall-dev <Melanie.Hall@parsons.com>
2021-06-23 13:05:53 -0500

    Added dynamic titles

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ClientApp';
-  dashboard = 'Dashboard';
+
+export class AppComponent implements OnInit {
+
+  title = 'Dashboard';
+  router: any;
+  activatedRoute: any;
+  
+  constructor(private titleService: Title) {}
+
+  setDocTitle(title: string) {
+    this.titleService.setTitle(title);
+  }
+
+  getDocTitle() {
+    return this.titleService.getTitle();
+  }
+
+  ngOnInit() {
+    const appTitle = this.titleService.getTitle();
+    this.router
+      .events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => {
+          const child = this.activatedRoute.firstChild;
+          if (child.snapshot.data['title']) {
+            return child.snapshot.data['title'];
+          }
+          return appTitle;
+        })
+      ).subscribe((ttl: string) => {
+        this.titleService.setTitle(ttl);
+    });
+  }
 }
+
+
