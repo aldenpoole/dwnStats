c2c3f8bf44eed0231a0edf54abfb84e1972dc985
SamSongy-Parsons <sam.songy@parsons.com>
2021-06-30 10:33:17 -0500

    Center data chart on dashboard page, made data chart's display larger, add for loop to set datachart's labels equal to past 24 hours

--- ClientApp/src/app/dashboard/data-downloaded-graph/data-downloaded-graph.component.ts
+++ ClientApp/src/app/dashboard/data-downloaded-graph/data-downloaded-graph.component.ts
@@ -8,15 +8,40 @@ import { Chart } from 'chart.js'
 })
 export class DataDownloadedGraphComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
+    const date = new Date();
+    let hour = date.getHours();
+
+    const hourLabels = [];
+    //loop through hours until comes back around 24;
+    for(let i=0; i<=24; i++) {
+
+      if(hour > 12) {
+        hour = hour - 12;
+      }
+
+      if(hour < 1) {
+        hour = hour + 12;
+      }
+
+      hourLabels.push(hour);
+      hour = hour - 1;
+    }
+
+    hourLabels.reverse();
+
+    console.log(hourLabels);
+
     const data = {
-      labels: ["January", "February", "March", "April", "May", "June",
-      "July"],
+      //have data fill past 24 hours with current hour being last item
+      labels: hourLabels,
       datasets: [{
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'Download in Past 24 Hours',
+        data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40,65, 59, 80, 81, 56, 55, 40, 56, 55, 40, 65],
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
